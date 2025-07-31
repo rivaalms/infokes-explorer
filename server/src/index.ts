3cfaeda363b6a51bf52445e7580ae613198a6823
Riva Almero <rvalms@icloud.com>
2025-07-31 19:07:01 +0700

    chore: init

---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from "elysia"
+
+const port = process.env.SERVER_PORT ?? 3000
+const app = new Elysia().get("/", () => "Hello Elysia").listen(port)
+
+console.log(
+   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
