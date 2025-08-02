d9075b9fded2d8784a55b77fca146fe051e83829
Riva Almero <rvalms@icloud.com>
2025-08-02 12:02:34 +0700

    refactor(app): alter file tree recursive logic

--- app/src/components/FolderTree.vue
+++ app/src/components/FolderTree.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
 import FolderTreeItem from "./FolderTreeItem.vue"
 import { defineProps } from "vue"
-import { useFileStore } from "@/stores/file"
 
 const props = defineProps<{
    data: Model.Folder[]
 }>()
-
-const fileStore = useFileStore()
-
-function onClick(item: Model.Folder) {
-   if (!item.children) {
-      alert(item.name)
-      return
-   }
-   fileStore.currentDir = item
-}
 </script>
 
 <template>
-   <template v-for="item in props.data">
-      <FolderTreeItem
-         :id="item.id"
-         :label="item.name"
-         :children="item.children"
-         @click.stop="onClick(item)"
-      />
-   </template>
+   <div class="flex flex-col">
+      <template v-for="item in props.data">
+         <FolderTreeItem
+            :item="item"
+         />
+      </template>
+   </div>
 </template>
--- app/src/components/FolderTreeItem.vue
+++ app/src/components/FolderTreeItem.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import FolderTree from "./FolderTree.vue"
 import { defineProps, shallowRef } from "vue"
+import { useFileStore } from "@/stores/file"
 
 const props = defineProps<{
-   id: number
-   label: string
-   children?: Model.Folder[]
+   item: Model.Folder
 }>()
 
 const isChildrenShow = shallowRef(false)
@@ -13,15 +11,25 @@ const isChildrenShow = shallowRef(false)
 function onChevronClick() {
    isChildrenShow.value = !isChildrenShow.value
 }
+
+const fileStore = useFileStore()
+function onClick(item: Model.Folder) {
+   if (!item.children) {
+      alert(item.name)
+      return
+   }
+   fileStore.currentDir = item
+}
 </script>
 
 <template>
    <div class="flex flex-col">
       <div
          class="flex items-center gap-2 px-4 py-2 hover:bg-blue-500/5 hover:text-blue-500 rounded-lg cursor-pointer"
+         @click.stop="onClick(props.item)"
       >
          <span
-            v-if="props.children && props.children.length > 0"
+            v-if="props.item.children && props.item.children.length > 0"
             class="lucide--chevron-down size-4"
             :class="{ '-rotate-90': !isChildrenShow }"
             @click.stop="onChevronClick"
@@ -29,12 +37,14 @@ function onChevronClick() {
          <span v-else class="size-4" />
          <span class="lucide--folder size-4" />
          <span class="text-sm font-medium flex-1">
-            {{ props.label }}
+            {{ props.item.name }}
          </span>
       </div>
-      <template v-if="props.children && isChildrenShow">
-         <div class="ms-4 flex flex-col gap-1">
-            <FolderTree :data="props.children" />
+      <template v-if="props.item.children && isChildrenShow">
+         <div class="ms-4 flex flex-col">
+            <template v-for="child in props.item.children">
+               <FolderTreeItem :item="child" />
+            </template>
          </div>
       </template>
    </div>
